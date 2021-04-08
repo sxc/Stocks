66082d06c3a2df21a0b135446918b181e266e481
sxc <qiuyequ@icloud.com>
2021-04-08 09:31:14 +0800

    launching to StockList

--- Shared/StocksApp.swift
+++ Shared/StocksApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct StocksApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            StockList()
         }
     }
 }
