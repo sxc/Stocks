1fe9346a14cbc601c413a87ee11614a0f49faaf8
sxc <qiuyequ@icloud.com>
2021-04-08 09:08:34 +0800

    Initial Commit

---
+++ Shared/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2021/4/8.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
